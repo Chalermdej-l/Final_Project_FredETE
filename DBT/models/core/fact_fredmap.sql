dfc5ece016005b88d4696e2d30c978c9cf479e2a
chalermdej.lematavekul@gmail.com <chalermdej.lematavekul@gmail.com>
2023-04-17 07:00:34 +0000

    Add type

--- DBT/models/core/fact_fredmap.sql
+++ DBT/models/core/fact_fredmap.sql
@@ -29,8 +29,19 @@ ifnull(co.continent,'Missing') as continent,
 ifnull(co.sub_region,'Missing') as sub_region ,
 co.latitude ,
 co.longitude,
-sg.title as grouptitle,
-s.title as seriestitle,
+replace(replace(replace(replace(replace(replace(sg.title ,'Gross Domestic Product','GDP'),'Harmonized Index of Consumer Prices','Price Index of'),'Amount Outstanding Due Within',''),'Amount Outstanding of Total Debt Securities','Total Debt'),'Amount Outstanding of International Debt Securities','International Debt'),'Net Issues of International Debt Securities for Issuers','Issues of International Debt') as grouptitle,
+case 
+when sg.title like '%index%' then 'index'
+when sg.title like '%GDP%' then 'GDP'
+when sg.title like '%Gross Domestic Product%' then 'GDP'
+when sg.title like '%Interest Rates%' then 'Interest Rates'
+when sg.title like '%Debt%' then 'Debt'
+when sg.title like '%Export%' then 'Trade'
+when sg.title like '%Import%' then 'Trade'
+when sg.title like '%Trade%' then 'Trade'
+when sg.title like '%Exchange Rate%' then 'Exchange Rate'
+else 'Other'
+end as type,
 s.frequency,
 m.value,
 s.units,
